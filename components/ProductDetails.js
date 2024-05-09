da204d756d673a9c86ab6287786a302453d54b0e
daniel morales <daniel_morales81@hotmail.commorales81@hotmail.com>
2024-05-08 21:18:54 -0400

    L9 completed

---
+++ components/ProductDetails.js
@@ -0,0 +1,18 @@
+app.component('product-details', {
+    props: {
+        details: {
+            type: Array,
+            required: true
+        }
+    },
+    template: 
+    `<ul>
+        <li v-for="detail in details">{{ detail }}</li>
+    </ul>`,
+    data() {
+        return {
+            details: ['50% cotton', '30% wool', '20% polyester']
+        }
+    }
+}
+)
